f7cd2494ec5a69b1c349bcac8153b27c3ab81381
Kecoak Binal <coro-binal@b2b.web.id>
2020-02-27 13:03:51 +0700

    .gitignore + unduh

---
+++ unduh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+wget -c https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_bahamas_leaks.2017-12-19.zip#_ga=2.119539201.8486707.1582771435-1856301087.1581924992 \
+     -O csv/csv_bahamas_leaks.2017-12-19.zip
+wget -c https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_offshore_leaks.2018-02-14.zip#_ga=2.178326829.8486707.1582771435-1856301087.1581924992 \
+     -O csv/csv_offshore_leaks.2018-02-14.zip
+wget -c https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_panama_papers.2018-02-14.zip#_ga=2.178326829.8486707.1582771435-1856301087.1581924992 \
+     -O csv/csv_panama_papers.2018-02-14.zip
+wget -c https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_paradise_papers.2018-02-14.zip#_ga=2.178326829.8486707.1582771435-1856301087.1581924992 \
+     -O csv/csv_paradise_papers.2018-02-14.zip
